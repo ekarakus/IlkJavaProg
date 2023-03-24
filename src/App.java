a89afd9bb76cb6c765f393d3fd91d0e05d2e08b1
Ergün Karakuş <ekarakus@btofis.com>
2023-03-24 11:39:49 +0300

    contruystor metodu kullanıkldı

--- src/App.java
+++ src/App.java
@@ -1,7 +1,14 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Merhaba dünya");
 
-        //
+    public static void main(String[] args) throws Exception {
+        
+        Telefon tel1 = new Telefon();
+        tel1.Marka = "Samsung";
+        tel1.Model = "A70";
+
+        Ogrenci ogrenci = new Ogrenci("Ecem",
+                "Aykanat",
+                1592);
+
     }
 }
---
+++ src/Ogrenci.java
@@ -0,0 +1,15 @@
+public class Ogrenci {
+
+    public Ogrenci(
+            String ad,
+            String soyad,
+            int numara) {
+        this.Ad = ad;
+        this.Soyad = soyad;
+        this.Numara = numara;
+    }
+
+    public int Numara;
+    public String Ad;
+    public String Soyad;
+}
